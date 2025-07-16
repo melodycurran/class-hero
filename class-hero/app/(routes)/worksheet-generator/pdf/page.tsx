bda7e7250bbf8091583fc16e59c8a76114417389
melodycurran <mcurran@byupathway.edu>
2025-07-15 21:26:31 -0600

    Fixed

--- class-hero/app/(routes)/worksheet-generator/pdf/page.tsx
+++ class-hero/app/(routes)/worksheet-generator/pdf/page.tsx
@@ -1,7 +1,13 @@
 'use client'
-import { Page, Text, View, Document, PDFViewer, PDFDownloadLink, StyleSheet } from "@react-pdf/renderer"
+import { Page, Text, View, Document } from "@react-pdf/renderer"
 import styles from "../../../page.module.css"
+import dynamic from "next/dynamic"
 
+const PDFViewer = dynamic(() => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+    {
+        ssr: false,
+        loading: () => <p>...Loading</p>
+    })
 
 export default function PdfView() {
 
