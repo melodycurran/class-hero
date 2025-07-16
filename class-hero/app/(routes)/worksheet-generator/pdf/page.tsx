12125efa0455051af8f05a459646304b9a70fb0a
melodycurran <mcurran@byupathway.edu>
2025-07-15 21:31:26 -0600

    Added useEffect

--- class-hero/app/(routes)/worksheet-generator/pdf/page.tsx
+++ class-hero/app/(routes)/worksheet-generator/pdf/page.tsx
@@ -1,26 +1,31 @@
 'use client'
-import { Page, Text, View, Document } from "@react-pdf/renderer"
+import { Page, Text, View, Document, PDFViewer } from "@react-pdf/renderer"
 import styles from "../../../page.module.css"
-import dynamic from "next/dynamic"
+import { useEffect, useState } from "react"
 
-const PDFViewer = dynamic(() => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
-    {
-        ssr: false,
-        loading: () => <p>...Loading</p>
-    })
 
 export default function PdfView() {
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        setLoaded(true)
+    }, [])
 
     return (
-        <PDFViewer className={styles.pdfViewer}>
-            <Document>
-                <Page size="A4" >
-                    <View >
-                        <Text>This is sample PDF</Text>
-                    </View>
-                </Page>
-            </Document>
-        </PDFViewer>
+        <>
+            {loaded && (
+                <PDFViewer className={styles.pdfViewer}>
+                    <Document>
+                        <Page size="A4" >
+                            <View >
+                                <Text>This is sample PDF</Text>
+                            </View>
+                        </Page>
+                    </Document>
+                </PDFViewer>
+            )}
+        </>
+
     )
 
 }
