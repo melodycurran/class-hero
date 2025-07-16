64b35e18a25527e320d5f3113d3d47fdc200e1da
melodycurran <mcurran@byupathway.edu>
2025-07-15 21:36:27 -0600

    NA

--- class-hero/app/(routes)/worksheet-generator/pdf/page.tsx
+++ class-hero/app/(routes)/worksheet-generator/pdf/page.tsx
@@ -1,19 +1,13 @@
 'use client'
 import { Page, Text, View, Document, PDFViewer } from "@react-pdf/renderer"
 import styles from "../../../page.module.css"
-import { useEffect, useState } from "react"
 
 
 export default function PdfView() {
-    const [loaded, setLoaded] = useState(false)
-
-    useEffect(() => {
-        setLoaded(true)
-    }, [])
 
     return (
         <>
-            {loaded && (
+            {typeof window !== 'undefined' && (
                 <PDFViewer className={styles.pdfViewer}>
                     <Document>
                         <Page size="A4" >
