4cd1989db6d7e9c6d01856e447606f0ed015a8bc
melodycurran <mcurran@byupathway.edu>
2025-08-14 00:24:31 -0600

    Fixed Deployment error

--- class-hero/app/(protected)/worksheet-editor/(dashboard)/page.tsx
+++ class-hero/app/(protected)/worksheet-editor/(dashboard)/page.tsx
@@ -1,17 +1,18 @@
-// import CanvasSize from "@/components/ui/canvasSize"
-// import RecentWorksheet from "@/components/ui/recentWorksheet"
+import CanvasSize from "@/components/ui/canvasSize"
+import RecentWorksheet from "@/components/ui/recentWorksheet"
 
 
 
-// export default async function WorksheetEditor() {
-    
-//     return (
-//         <div className="flex flex-col font-sans text-sm gap-4 ">
-//             <h2 className="font-bold">Worksheet Editor</h2>
-//             <section>
-//                 <CanvasSize />
-//             </section>
-//             <RecentWorksheet />
-//         </div>
-//     )
-// }
+export default async function WorksheetEditor() {
+
+    return (
+        // <div className="flex flex-col font-sans text-sm gap-4 ">
+        //     <h2 className="font-bold">Worksheet Editor</h2>
+        //     <section>
+        //         <CanvasSize />
+        //     </section>
+        //     <RecentWorksheet />
+        // </div>
+        <div></div>
+    )
+}
